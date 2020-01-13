666c6384e66f7f70cb6c9762ea55cc8a527722c7
sarahdepaz <sarahdepaz27@gmail.com>
2020-01-13 20:35:37 +0200

    servers

---
+++ MyParallelServer.h
@@ -0,0 +1,32 @@
+
+#ifndef BIUPROJECT2_MYPARALLELSERVER_H
+#define BIUPROJECT2_MYPARALLELSERVER_H
+
+#include <pthread.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <thread>
+#include <sstream>
+#include <mutex>
+#include "Server.h"
+#include "ClientHandler.h"
+#include "ArgumentsForOpenServer.h"
+
+using namespace server_side;
+static std::mutex mutex;
+
+class MyParallelServer : public Server {
+ private:
+  /* Holds all the opened pthreads. */
+  std::vector<pthread_t *> threads;
+
+ public:
+  void open(int port, ClientHandler *clientHandler) override;
+  void stop() override;
+
+ private:
+  static void *callHandler(void *);
+};
+
+#endif //BIUPROJECT2_MYPARALLELSERVER_H
---
+++ Server.h
@@ -0,0 +1,35 @@
+
+#ifndef BIUPROJECT2_SERVER_H
+#define BIUPROJECT2_SERVER_H
+
+#include <sys/socket.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+#include "ClientHandler.h"
+
+/**
+ * Define's a serve.
+ */
+namespace server_side {
+class Server {
+ public:
+
+  /**
+   * Open's the server in a specific port and calls a handler.
+   * @param port The port to use.
+   * @param clientHandler The handler to use.
+   */
+  virtual void open(int port, ClientHandler *clientHandler) = 0;
+
+  /**
+   * Stop's the server.
+   */
+  virtual void stop() = 0;
+};
+}
+
+#endif //BIUPROJECT2_SERVER_H
