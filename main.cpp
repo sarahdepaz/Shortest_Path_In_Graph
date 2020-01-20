733c78725b5644d9052f7fd4dd037b93996fb584
sarahdepaz <sarahdepaz27@gmail.com>
2020-01-20 13:27:27 +0200

    changed port from client

--- main.cpp
+++ main.cpp
@@ -25,13 +25,13 @@ std::string GetPath(State<std::pair<int, int>> *go) {
     std::pair<int, int> current_pos = current->getState();
     std::pair<int, int> previous_pos = previous->getState();
     if (current_pos.first == previous_pos.first + 1) {
-      result.insert(0, ",Down (" + std::to_string((int)current->getTotal_cost_to())+") ");
+      result.insert(0, ",Down (" + std::to_string((int) current->getTotal_cost_to()) + ") ");
     } else if (current_pos.first == previous_pos.first - 1) {
-      result.insert(0, ",Up (" + std::to_string((int)current->getTotal_cost_to())+") ");
+      result.insert(0, ",Up (" + std::to_string((int) current->getTotal_cost_to()) + ") ");
     } else if (current_pos.second == previous_pos.second - 1) {
-      result.insert(0, ",Left (" + std::to_string((int)current->getTotal_cost_to())+") ");
+      result.insert(0, ",Left (" + std::to_string((int) current->getTotal_cost_to()) + ") ");
     } else if (current_pos.second == previous_pos.second + 1) {
-      result.insert(0, ",Right (" + std::to_string((int)current->getTotal_cost_to())+") ");
+      result.insert(0, ",Right (" + std::to_string((int) current->getTotal_cost_to()) + ") ");
     }
     current = previous;
     previous = current->getCameFrom();
@@ -45,28 +45,18 @@ std::string GetPath(State<std::pair<int, int>> *go) {
 int main(int argc, char **argv) {
   /* Trying to get port from client. */
   int port;
-  if (argc == 1) {
-    exit(0);
-  } else if (argc == 2) {
-    try {
-      port = std::stoi(argv[1]);
-      // todo check with roi if will be supplied in main
-    } catch (std::exception &e) {
-      exit(0);
-    }
+  if (argc == 2) {
+    port = std::stoi(argv[1]);
   } else {
-    exit(0);
+    port = 5600;
   }
-
   /* Creates searcher as a solver. */
   ISearcher<std::pair<int, int>, State<std::pair<int, int>> *> *searcher;
   //searcher = new BestFirstSearch<std::pair<int, int>, State<std::pair<int, int>> *>;
   //searcher = new BreadthFirstSearch<std::pair<int, int>, State<std::pair<int, int>> *>;
   searcher = new AStar<std::pair<int, int>, State<std::pair<int, int>> *>;
-
   Solver<ISearchable<std::pair<int, int>> *, State<std::pair<int, int>> *> *solver
       = new SolverToSearcherAdapter(searcher);
-
   /* Creates CacheManager and ClientHandler. */
   CacheManager *cacheManager = new FileCacheManager("matrix.txt");
   MyClientHandler myClientHandler(solver, cacheManager);
@@ -80,4 +70,5 @@ int main(int argc, char **argv) {
   delete (solver);
   delete (cacheManager);
 
+  return 0;
 };
